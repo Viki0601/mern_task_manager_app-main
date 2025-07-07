1b9cf7f2b51fed26dce0886d654cf7aab861d0c2
Vignesh <153413717+Viki0601@users.noreply.github.com>
2025-07-07 09:58:59 +0530

    Update firebase.js

--- To-do-App/frontend/src/firebase.js
+++ To-do-App/frontend/src/firebase.js
@@ -1,19 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "todo-task-manager-68372.firebaseapp.com",
-  projectId: "todo-task-manager-68372",
-  storageBucket: "todo-task-manager-68372.appspot.com",
-  messagingSenderId: "979959198551",
-  appId: "1:979959198551:web:a6f8f567195fb965220412",
-  measurementId: "G-PJSRCWCKVQ"
+  apiKey: "AIzaSyBazXeN5S98J82zkBTzuXCbYN0CD0UM95M",
+  authDomain: "mern-task-manager-app-3c8d2.firebaseapp.com",
+  projectId: "mern-task-manager-app-3c8d2",
+  storageBucket: "mern-task-manager-app-3c8d2.appspot.com", // ✅ fixed
+  messagingSenderId: "27133231727",
+  appId: "1:27133231727:web:552ca6e32ec5212f10c39a",
+  measurementId: "G-JFGV5MCWCW"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
 
 export { app, auth, provider };
+
